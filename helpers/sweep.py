76b88677064c8c044a8bb7ae8331142ec113dc34
KrisHan999 <khan7@uci.edu>
2019-05-27 10:02:58 -0700

    final_path_genenration

--- helpers/sweep.py
+++ helpers/sweep.py
@@ -1,10 +1,10 @@
-from geometry import *
+from helpers.geometry import *
 import math
-
 # return list of points
-def sweep(vertexes):
+def sweep(vertexes, width, step):
     # width of tuolaji
-    WIDTH = 2
+    WIDTH = width
+    STEP = step
 
     edges = []
     maxLength = -1
@@ -45,16 +45,19 @@ def sweep(vertexes):
 
         longestEdge = longestEdge.shift(WIDTH)
 
-    for i in intersections:
-        print(i)
-
     reorderIntersections(edges, intersections)
 
-    print("-----------------------")
-    for i in intersections:
-        print(i)
+    # for i in intersections:
+    #     print(i)
 
-    return intersections
+    res = addStepsToPath(intersections, STEP)
+
+    # print("-------------------")
+
+    # for i in res:
+    #     print(i)
+
+    return res
 
 
 def reorderIntersections(edges, intersections):
@@ -78,20 +81,70 @@ def reorderIntersections(edges, intersections):
 
     direct = 1
     i = 0
+
     # direct == 1, intersection on major edge at the front
     # direct == -1, intersection on major edge at the back
     while i < len(intersections):
-        if direct == 1 & majorEdge.relativePosition(intersections[i]) != 0:
+        if direct == 1 and majorEdge.relativePosition(intersections[i]) != 0:
             temp = intersections[i]
             intersections[i] = intersections[i + 1]
             intersections[i + 1] = temp
-        elif direct == -1 & majorEdge.relativePosition(intersections[i + 1]) != 0:
+        elif direct == -1 and majorEdge.relativePosition(intersections[i + 1]) != 0:
             temp = intersections[i]
             intersections[i] = intersections[i + 1]
             intersections[i + 1] = temp
 
         direct = 0 - direct
         i = i + 2
+
+
+def addStepsToPath(path, step):
+    res = []
+
+    i = 0
+    while i < len(path) - 1:
+        inter0 = path[i]
+        inter1 = path[i + 1]
+
+        steps = stepsBetwn(inter0, inter1, step)
+
+        res.append(inter0)
+        res.extend(steps)
+        i += 1
+
+    res.append(path[len(path) - 1])
+    if len(res) >= 2 and res[len(res) - 1].equals(res[len(res) - 2]):
+        res.pop(len(res) - 1)
+
+    return res
+
+# res doesn't contain two endpoints
+def stepsBetwn(inter0, inter1, step):
+    res = []
+    if inter0.equals(inter1):
+        return res
+
+    line = Line.getLineFromTwoPoints(inter0, inter1)
+    xDirect = 1 if inter0.x < inter1.x else -1
+    yDirect = 1 if inter0.y < inter1.y else -1
+
+    if line.vertical:
+        nextY = round(inter0.y + yDirect * step, 2)
+
+        while (yDirect == 1 and nextY < inter1.y) or (yDirect == -1 and nextY > inter1.y):
+            res.append(point(inter0.x, nextY))
+            nextY = round(nextY + yDirect * step, 2)
+    else:
+        nextX = round(inter0.x + xDirect * (step / math.sqrt(1 + line.k ** 2)), 2)
+        nextY = round(inter0.y + yDirect * (step * abs(line.k) / math.sqrt(1 + line.k ** 2)), 2)
+
+        while (xDirect == 1 and nextX < inter1.x) or (xDirect == -1 and nextX > inter1.x):
+            res.append(point(nextX, nextY))
+            nextX = round(nextX + xDirect * (step / math.sqrt(1 + line.k ** 2)), 2)
+            nextY = round(nextY + yDirect * (step * abs(line.k) / math.sqrt(1 + line.k ** 2)), 2)
+
+    return res
+
 
 class Line:
     def __init__(self, vertical, x, k = -1, b = -1, minX = -1, minY = -1, maxX = -1, maxY = -1):
@@ -106,12 +159,12 @@ class Line:
 
 
     def equals(self, line1):
-        if self.vertical & line1.vertical:
+        if self.vertical and line1.vertical:
             return self.x == line1.x
-        elif self.vertical | line1.vertical:
+        elif self.vertical or line1.vertical:
             return False
         else:
-            return self.k == line1.k & self.b == line1.b
+            return self.k == line1.k and self.b == line1.b
 
     @staticmethod
     def getLineFromTwoPoints(point1, point2):
@@ -135,9 +188,9 @@ class Line:
         b2 = line2.b
 
 
-        if self.vertical & line2.vertical:
+        if self.vertical and line2.vertical:
             return None
-        elif self.vertical | line2.vertical:
+        elif self.vertical or line2.vertical:
             if self.vertical:
                 x = self.x
                 y = round(line2.k * x + line2.b, 2)
@@ -150,7 +203,7 @@ class Line:
             x = round((b2 - b1) / (k1 - k2), 2)
             y = round((b2 * k1 - b1 * k2) / (k1 - k2), 2)
 
-        if (x >= line2.minX) & (x <= line2.maxX) & (y >= line2.minY) & (y <= line2.maxY):
+        if (x >= line2.minX) and (x <= line2.maxX) and (y >= line2.minY) and (y <= line2.maxY):
             return point(x, y)
         else:
             return None
@@ -193,7 +246,7 @@ class Line:
 
 
     def length(self):
-        if self.minX == -1 & self.minY == -1 & self.maxX == -1 & self.maxY == -1:
+        if self.minX == -1 and self.minY == -1 and self.maxX == -1 and self.maxY == -1:
             return -1
         else:
             return math.sqrt((self.maxX - self.minX) ** 2 + (self.maxY - self.minY) ** 2)
@@ -204,33 +257,6 @@ class Line:
         self.minY = -1
         self.maxX = -1
         self.maxY = -1
-
-
-
-
-
-
-
-
-
-def main():
-    point0 = point(3, 2)
-    point1 = point(9, 8)
-    point2 = point(2, 9)
-    point3 = point(1, 8)
-
-    list = []
-    list.append(point0)
-    list.append(point1)
-    list.append(point2)
-    list.append(point3)
-
-    sweep(list)
-
-
-
-
-# main()
 
 
 
